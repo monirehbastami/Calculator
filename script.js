4e97ba2377aa631223a5a68fb8c7ba2db9eaa03c
MonirehBastami <monirehbastami@gmail.com>
2022-12-25 09:52:54 -0800

    change interface

--- script.js
+++ script.js
@@ -7,32 +7,41 @@ window.addEventListener('load', function () {
     const opt = ['+','-','*','/'];
     nodeList.forEach((item)=> {
         item.addEventListener("click",()=>{
-            const len = nodeArr.length;
-            console.log(item.value);
+            let strval='';
+            let len = nodeArr.length;
             let item_val = item.value;
-            if(item_val == "C"){
-                nodeArr=[];
-                inputRes.value = '';
-            }else{
-                if(checkDigitDot(item_val)){
+            let last_item = nodeArr[len - 1];
+            if(checkDigitDot(item_val)){
+                if(item_val == '.'){
+                    if(!nodeArr.includes(".")){
+                        nodeArr.push(item_val);
+                        inputRes.value += item_val;
+                    }
+                }else if(last_item == '='){
+                    nodeArr=[];
+                    nodeArr.push(item_val);
+                    inputRes.value = item_val;
+                }else{
                     nodeArr.push(item_val);
                     inputRes.value += item_val;
-                    //this.alert(item_val)
+                }
+            }else{
+                if(nodeArr.length == 0){
+                    this.alert('Insert a number');
+                    return false;
                 }else{
-                    if(nodeArr.length == 0){
-                        this.alert('Insert a number');
+                    if(item_val == "C"){
+                        nodeArr=[];
+                        inputRes.value = '';
                         return false;
+                    }
+                    let index_item = nodeArr.indexOf(nodeArr.find((thisnode)=> opt.includes(thisnode)));
+                    if(opt.includes(last_item) || last_item == "="){
+                        nodeArr[len-1] = item_val;
+                        strval  = nodeArr.join('');
+                        inputRes.value = strval.replace("=","");
                     }else{
-                        let index_item = nodeArr.indexOf(nodeArr.find((thisnode)=> opt.includes(thisnode)));
-                        if(index_item == -1){
-                            let newnumber = nodeArr.join('');
-                            nodeArr=[];
-                            nodeArr.push(newnumber);
-                            nodeArr.push(item_val);
-                            if(item_val == "=") inputRes.value = newnumber;
-                            else
-                            inputRes.value = newnumber+item_val;
-                        }else{
+                        if(checkDigitDot(last_item) && (index_item > -1)){
                             var numBeforOpt = '';
                             for(i = index_item+1; i<len;i++){
                                 numBeforOpt += nodeArr[i];
@@ -45,47 +54,48 @@ window.addEventListener('load', function () {
                             let stac_opt = nodeArr.pop();
                             let fisrt_number = nodeArr.pop();
                             let current_res = getCurrentRes(fisrt_number,stac_opt,second_num);
-                            if(item_val == "="){
-                                inputRes.value = current_res;
-                                nodeArr=[];
-                                nodeArr.push(current_res);
-                            }else{
-                                inputRes.value = current_res+item_val;
-                                nodeArr.push(current_res);
-                                nodeArr.push(item_val);
-                            }
+                            nodeArr = [];
+                            nodeArr.push(current_res);
+                            nodeArr.push(item_val);
+                            strval  = nodeArr.join('');
+                            inputRes.value = strval.replace("=","");
+                        }else if(checkDigitDot(last_item) && (index_item == -1)){
+                           // this.alert(last_item);
+                            let newnumber = nodeArr.join('');
+                            nodeArr = [];
+                            nodeArr.push(newnumber);
+                            nodeArr.push(item_val);
+                            strval  = nodeArr.join('');
+                            inputRes.value = strval.replace("=","");
                         }
-                        
-                        
                     }
-                }
-            }
-            
+                } 
+            }           
             
         })
-        
     });
-  function checkDigitDot(item){
-    let isDigit = reg.test(item);
-    let digdotRes = (isDigit || item == '.') ? true : false;
-    return digdotRes;
-  }
-  function getCurrentRes(fisrt_number,stac_opt,second_num){
-    var getCurRes = 0;
-    switch(stac_opt){
-        case '+':
-            getCurRes = parseInt(fisrt_number) + parseInt(second_num);
-        break;
-        case '-':
-            getCurRes = parseInt(fisrt_number) - parseInt(second_num);
-        break;
-        case '*':
-            getCurRes = parseInt(fisrt_number) * parseInt(second_num);
-        break;
-        case '/':
-            getCurRes = parseInt(fisrt_number) / parseInt(second_num);
-        break;
+
+    function checkDigitDot(item){
+        let isDigit = reg.test(item);
+        let digdotRes = (isDigit || item == '.' || (parseFloat(item) > 0)) ? true : false;
+        return digdotRes;
     }
-    return getCurRes;
-  }
- });
+    function getCurrentRes(fisrt_number,stac_opt,second_num){
+        var getCurRes = 0;
+        switch(stac_opt){
+            case '+':
+                getCurRes = parseFloat(fisrt_number) + parseFloat(second_num);
+            break;
+            case '-':
+                getCurRes = parseFloat(fisrt_number) - parseFloat(second_num);
+            break;
+            case '*':
+                getCurRes = parseFloat(fisrt_number) * parseFloat(second_num);
+            break;
+            case '/':
+                getCurRes = parseFloat(fisrt_number) / parseFloat(second_num);
+            break;
+        }
+        return getCurRes;
+    }
+});
